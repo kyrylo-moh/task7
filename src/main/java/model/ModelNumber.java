552fd0903500f4067afaa0a394e91b69665af9f3
kimoe <kimo.evil@gmail.com>
2019-10-27 13:32:29 +0200

    Added class ModelNumber and his get set and contructor

---
+++ src/main/java/model/ModelNumber.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ModelNumber {
+
+    private int number;
+
+    public ModelNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
